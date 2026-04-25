228613eaa75341094952a23ac42315cbca9278b7
Jose Alvarez <alvarez.jos3.es@gmail.com>
2026-04-25 16:41:01 +0200

    Revert "ChemaModsV2"
    This reverts commit 71d36a1910b8a5edca79325768ca9b9ce056fd56.

--- src/gta/script_handler.cpp
+++ src/gta/script_handler.cpp
@@ -6,18 +6,22 @@
 
 int CGameScriptHandlerNetComponent::get_participant_index(CNetGamePlayer* player)
 {
-	for (auto* p = m_first_participant; p; p = p->m_next)
-		if (p->m_net_game_player == player)
-			return p->m_participant_index;
+	if (player == (*big::g_pointers->m_gta.m_network_player_mgr)->m_local_net_player)
+		return m_local_participant_index;
+
+	if (m_max_participants <= 1)
+		return -1;
+
+	for (int i = 0; i < m_max_participants - 1; i++)
+	{
+		if (m_participants[i] && m_participants[i]->m_net_game_player == player)
+			return m_participants[i]->m_participant_index;
+	}
 
 	return -1;
 }
 
 bool CGameScriptHandlerNetComponent::is_player_a_participant(CNetGamePlayer* player)
 {
-	for (auto* p = m_first_participant; p; p = p->m_next)
-		if (p->m_net_game_player == player)
-			return true;
-
-	return false;
+	return m_participant_bitset & (1 << player->m_player_id);
 }
