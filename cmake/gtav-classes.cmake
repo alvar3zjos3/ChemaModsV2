23ca9d3b25fa68a605e56d78d069accdecee59a3
Jose Alvarez <alvarez.jos3.es@gmail.com>
2026-04-25 16:53:24 +0200

    ChemaModsV2

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/Mr-X-GTA/GTAV-Classes-1.git
-    GIT_TAG        9fa80bd650d37afd70056ec0f0c987cbc62e8a74
+    GIT_REPOSITORY https://github.com/alvar3zjos3/GTAV-Classes-1.git
+    GIT_TAG        main
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
