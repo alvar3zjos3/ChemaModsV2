71d36a1910b8a5edca79325768ca9b9ce056fd56
Jose Alvarez <alvarez.jos3.es@gmail.com>
2026-04-25 15:11:10 +0200

    ChemaModsV2

--- src/gta/script_handler.cpp
+++ src/gta/script_handler.cpp
@@ -6,22 +6,18 @@
 
 int CGameScriptHandlerNetComponent::get_participant_index(CNetGamePlayer* player)
 {
-	if (player == (*big::g_pointers->m_gta.m_network_player_mgr)->m_local_net_player)
-		return m_local_participant_index;
-
-	if (m_max_participants <= 1)
-		return -1;
-
-	for (int i = 0; i < m_max_participants - 1; i++)
-	{
-		if (m_participants[i] && m_participants[i]->m_net_game_player == player)
-			return m_participants[i]->m_participant_index;
-	}
+	for (auto* p = m_first_participant; p; p = p->m_next)
+		if (p->m_net_game_player == player)
+			return p->m_participant_index;
 
 	return -1;
 }
 
 bool CGameScriptHandlerNetComponent::is_player_a_participant(CNetGamePlayer* player)
 {
-	return m_participant_bitset & (1 << player->m_player_id);
+	for (auto* p = m_first_participant; p; p = p->m_next)
+		if (p->m_net_game_player == player)
+			return true;
+
+	return false;
 }
